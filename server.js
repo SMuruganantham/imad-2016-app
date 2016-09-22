d209502b3c611ff5a3c1cf02998b462365470a30
SMuruganantham <cs.muruganantham@gmail.com>
2016-09-22 16:52:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-app.get('/ui/Muru_0001.jpg',function(req,res){
+app.get('/Muru_0001.jpg',function(req,res){
       res.sendFile(path.join(__dirname,'ui','Muru_0001.jpg'));
 });
 
